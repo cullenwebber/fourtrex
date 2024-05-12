b41c91605008521b9623c335f4f7a959b4d19eaa
cullenwebber <139840023+cullenwebber@users.noreply.github.com>
2024-05-12 10:56:27 +0800

    Main

---
+++ src/app/ui/fonts.js
@@ -0,0 +1,11 @@
+import localFont from 'next/font/local'
+
+export const yapari = localFont({
+    src: '../../../public/fonts/Yapari-Bold.woff',
+    display: 'swap',
+})
+
+export const neueMontreal = localFont({
+    src: '../../../public/fonts/Neue-Montreal.otf',
+    display: 'swap',
+});
